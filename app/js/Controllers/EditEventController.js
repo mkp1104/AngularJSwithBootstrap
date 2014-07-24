9c127a54a837a84c2add4246faae391f730d5262
unknown <Manishp@Manishp01.emids.local>
2014-07-24 18:27:10 +0530

    newEditDetails Added by Manish

---
+++ app/js/Controllers/EditEventController.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+App.controller('EditEventController', function EditEventController($scope) {
+
+  $scope.SaveEvent = function (event, newEventForm) {
+
+    console.log(newEventForm);
+    if (newEventForm.$valid) {
+      window.alert('event' + event.name + 'saved!');
+    }
+    else {
+      window.alert('Form is not Valid!!!!');
+    }
+  };
+  $scope.EventEditCancel = function () {
+
+    window.location = 'EditEventDetails.htm';
+  };
+
+});
--- app/js/Controllers/EventController.js
+++ app/js/Controllers/EventController.js
@@ -1,48 +1,72 @@
 ﻿'use strict';
 var App = angular.module('eventsApp', ['ngSanitize']);
-App.controller('EventController', function EventController($scope) {
+//App.filter('durationFilter', function () {
+//  return function (duration) {
+//    switch (duration) { 
+//    case 2 : 
+//    return "Two Hour";
+//    case 5 : 
+//    return "Five Hour";
+//    case 9 : 
+//    return "Nine Hour";
+//    }
+
+//  }
+
+//});
+App.controller('EventController', function EventController($scope, eventData) {
+
   $scope.snippet = '<span style="color:red">Hi Dear</span>';
   $scope.boolvalue = true;
   $scope.myStyle = { color: 'red' };
   $scope.myClass = "Blue";
   $scope.buttonDisable = true;
-  $scope.sortorder = 'name';
+//  $scope.sortorder = 'name';
+  $scope.event = eventData.event;
+  //  $scope.event = {
+  //    name: 'Angular Js',
+  //    date: '1/2/2010',
+  //    time: '10:30 am',
+  //    location: {
+  //      address: 'Google HeadQuarters',
+  //      city: 'Mountain View(C.A)',
+  //      province: '(U.S.A)'
+  //    },
+  //    imageUrl: 'img/anguralBlue.png',
+  //    /// <reference path="../../img/anguralBlue.png" />/// <reference path="../../img/anguralBlue.png" />
 
-  $scope.event = {
-    name: 'Angular Js',
-    date: '1/2/2010',
-    time: '10:30 am',
-    location: {
-      address: 'Google HeadQuarters',
-      city: 'Mountain View(C.A)',
-      province: '(U.S.A)'
-    },
-    imageUrl: 'img/anguralBlue.png',
-    /// <reference path="../../img/anguralBlue.png" />/// <reference path="../../img/anguralBlue.png" />
 
+  //    sessions: [
+  //  {
+  //    name: 'Manish Prasad Advanced',
+  //    age: '23',
+  //    address: 'India(Earth)',
+  //    duration: 2, 
+  //    level: 'Advanced',
+  //    
+  //    upVoteCount: 3
+  //  },
+  //  {
+  //    name: 'Raghav Sinha Introductory',
+  //    age: '23',
+  //    address: 'India(Earth)',
+  //    duration: 5, 
+  //    level: 'Introductory',
+  //   
+  //    upVoteCount: 8
+  //  },
+  //  {
+  //    name: 'Salman Khan',
+  //    age: '23',
+  //    address: 'India(Earth)',
+  //    duration: 9, 
+  //    level: 'Intermediate',
+  //    
+  //    upVoteCount: 10
+  //  }
+  //  ]
 
-    sessions: [
-  {
-    name: 'Manish Prasad',
-    age: '23',
-    address: 'India(Earth)',
-    upVoteCount: 3
-  },
-  {
-    name: 'Raghav Sinha',
-    age: '23',
-    address: 'India(Earth)',
-    upVoteCount: 8
-  },
-  {
-    name: 'Salman Khan',
-    age: '23',
-    address: 'India(Earth)',
-    upVoteCount: 10
-  }
-  ]
-
-  }
+  //  }
 
   $scope.upVoteSession = function (session) {
     session.upVoteCount++;
